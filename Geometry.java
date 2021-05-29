a50ae542c532977989de8ffbe1124d1b3c5781db
shiracrammer <51131237+shiracrammer@users.noreply.github.com>
2021-05-29 21:34:25 +0300

    Update Geometry.java

--- Geometry.java
+++ Geometry.java
@@ -1,4 +1,6 @@
 package geometries;
+import Primitives.Color;
+import Primitives.Material;
 
 import java.util.List;
 
@@ -6,10 +8,51 @@ import Primitives.Point3D;
 import Primitives.Ray;
 import Primitives.Vector;
 
-public interface Geometry extends Intersectable
+public abstract class  Geometry implements Intersectable
 {
-public Vector getNormal(Point3D point) throws Exception;
 
-public List<Point3D> findIntsersections(Ray ray) throws Exception ;
+protected Color _emmission;
+protected Material _material;
 
+public  Geometry(Color color, Material material)
+{
+	_emmission=color;
+	_material=material;
+}
+
+
+/**
+ * @return the _material
+ */
+public Material get_material() {
+	return _material;
+}
+
+public abstract Vector getNormal(Point3D point) throws Exception;
+
+public abstract List<GeoPoint> findIntsersections(Ray ray) throws Exception ;
+
+/**
+ * @return the _emmission
+ */
+public Color get_emmission() {
+	return _emmission;
+}
+/*
+ * constructor
+ */
+public  Geometry(Color color)
+{
+//this(color, new Material(0.0, 0.0, 0));
+	_emmission=color;
+	_material= new Material(0.0, 0.0, 0);
+	
+}
+/*
+ * default constructor
+ */
+public  Geometry()
+{
+	_emmission=Color.BLACK;
+}
 }
